7b4c58d1dd8e50637006ef7c38b9cdfac7eb3762
AliRNamvar <68304636+AliRNamvar@users.noreply.github.com>
2020-09-21 00:25:02 +0330

    uncompleted file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Vigenere cipher
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 
 def key_generator(keyword, len_plaintext=0):
@@ -13,6 +14,22 @@ def key_generator(keyword, len_plaintext=0):
                 break
 
     return key_
+
+
+def generate_encrypt_table(alphabet):
+
+    output = []
+    for i in range(0, 26):
+        for j in alphabet:
+            output[i].append(j)
+
+        pop_val = alphabet.pop(0)
+        alphabet.append(pop_val)
+
+    print(output)
+
+
+generate_encrypt_table(alphabet)
 
 
 key = key_generator(keyword='AYUSH', len_plaintext=len('GEEKSFORGEEKS'))
