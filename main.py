42ac4e049425eccbce7be6dae32f99a4b434c5b7
AliRNamvar <68304636+AliRNamvar@users.noreply.github.com>
2020-09-20 22:55:32 +0430

    main file added

---
+++ main.py
@@ -0,0 +1,18 @@
+# Vigenere cipher
+
+
+def key_generator(keyword, len_plaintext=0):
+
+    key_ = ''
+    cont = True
+    while cont:
+        for i in keyword:
+            key_ = key_ + i
+            if len(key_) == len_plaintext:
+                cont = False
+                break
+
+    return key_
+
+
+key = key_generator(keyword='AYUSH', len_plaintext=len('GEEKSFORGEEKS'))
