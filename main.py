b7cac60f36d19fbf24ab38027a49c32aaf81f29c
AliRNamvar <68304636+AliRNamvar@users.noreply.github.com>
2020-09-21 18:25:45 +0330

    encryption function completed:)

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 # secondary branch
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+input_text = 'geeksforgeeks'
 
 
 def key_generator(keyword, len_plaintext=0):
-
     key_ = ''
     cont = True
     while cont:
@@ -19,7 +19,6 @@ def key_generator(keyword, len_plaintext=0):
 
 
 def generate_encrypt_table(alphabet):
-
     input = []
     for i in alphabet:
         input.append(i)
@@ -40,7 +39,28 @@ def generate_encrypt_table(alphabet):
     return output
 
 
+def encryption(key_encrypt=None, plain_text=None, encrypt_table=None):
+    alphabet_dic = dict(a=1, b=2, c=3, d=4, e=5, f=6, g=7, h=8, i=9, j=10, k=11, l=12, m=13, n=14, o=15, p=16, q=17, r=18, s=19, t=20, u=21, v=22, w=23, x=24, y=25, z=26)
+    ciphertext = []
+    index_key = 0
+    # key_encrypt[index_key]
+    for i in plain_text:
+        for j in encrypt_table:
+            # print("J:", j)
+
+            if j[0] == i:
+                for k, v in alphabet_dic.items():
+                    if key_encrypt[index_key] == k:
+                        # print("k in", k)
+                        v -= 1
+                        ciphertext.append(j[v])
+                        index_key += 1
+                        break
+    return ciphertext
+
+
 generate_encrypt_table(alphabet)
+key = key_generator(keyword='ayush', len_plaintext=len('geeksforgeeks'))
 
-
-key = key_generator(keyword='AYUSH', len_plaintext=len('GEEKSFORGEEKS'))
+encrypted_text = ''.join(encryption(key_encrypt=key, plain_text=input_text, encrypt_table=generate_encrypt_table(alphabet)))
+print(encrypted_text)
