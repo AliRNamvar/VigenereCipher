58cb80c7c8ebce73d21c923f0a30ba1d1e527bfe
AliRNamvar <68304636+AliRNamvar@users.noreply.github.com>
2020-09-25 23:22:33 +0330

    test deleted!

--- test.py
+++ test.py
@@ -4,3 +4,18 @@ alphabet_dic = dict(a=1, b=2, c=3, d=4, e=5, f=6, g=7, h=8, i=9, j=10, k=11, l=1
 
 for k, v in alphabet_dic.items():
     print('{}:{}'.format(k, v))
+
+
+while unvalid_input:
+
+    if situation1.lower() == "encryption" or situation1.lower() == "decryption":
+        unvalid_input = False
+
+    else:
+        situation1 = input("Unvalid input!\nTry again: ")
+
+
+if situation1.lower() == "encryption":
+    print("Your text: ", input_text, "\nEncrypted text: ", encrypted_text, sep='')
+elif situation1.lower() == "decryption":
+    print("Your text: ", input_text, "\nDecrypted text: ", decrypted_text, sep='')
