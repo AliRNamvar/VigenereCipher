db91a66edf42738206b1e15b183305af554fca8d
AliRNamvar <68304636+AliRNamvar@users.noreply.github.com>
2020-09-25 20:43:59 +0330

    decryption func not completed 1

--- main.py
+++ main.py
@@ -58,11 +58,44 @@ def encryption(key_encrypt=None, plain_text=None, encrypt_table=None):
                         ciphertext.append(j[v])
                         index_key += 1
                         break
-    return ciphertext
+    return ''.join(ciphertext)
+
+
+def decryption(encrypt_text, encrypt_table, key_encrypt):
+
+    # alphabet_dic = dict(a=1, b=2, c=3, d=4, e=5, f=6, g=7, h=8, i=9, j=10,
+    #                    k=11, l=12, m=13, n=14, o=15, p=16, q=17, r=18,
+    #                    s=19, t=20, u=21, v=22, w=23, x=24, y=25, z=26)
+    decryption_ = []
+    index_key = 0
+    for i in encrypt_text:
+        for j in encrypt_table:
+
+            in_k = 0
+            for k in key_encrypt:
+
+                if j[0] == k and in_k == index_key:
+
+                    for num in range(26):
+                        if j[num] == i:
+                            decryption_.append(encrypt_table[0][num])
+                            index_key += 1
+                            break
+                    # in_k += 1
+                else:
+                    in_k += 1
+
+
+
+
+    return ''.join(decryption_)
 
 
 generate_encrypt_table(alphabet)
 key = key_generator(keyword='ayush', len_plaintext=len('geeksforgeeks'))
+# print(key)
 
-encrypted_text = ''.join(encryption(key_encrypt=key, plain_text=input_text, encrypt_table=generate_encrypt_table(alphabet)))
-print(encrypted_text)
+encrypted_text = encryption(key_encrypt=key, plain_text=input_text, encrypt_table=generate_encrypt_table(alphabet))
+# print(encrypted_text)
+decrypted_text = decryption(encrypted_text, generate_encrypt_table(alphabet), key)
+print(decrypted_text)
